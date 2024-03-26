c5ccc14965aa5a15f2f2ebb8170bac5d5192e09b
Isox90 <step1kon@gmail.com>
2024-03-26 01:06:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Utwórz mapę
+const map = L.map('map').setView([50.264173, 18.890753], 10); // Przykładowe centrowanie na regionie Polski
+
+// Dodaj warstwę map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Załaduj dane JSON
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        // Dodaj markery z danych
+        data.forEach(point => {
+            L.marker([point.location.y, point.location.x], {
+                title: point.type // Ustawienie tytułu markera
+            }).addTo(map);
+        });
+    });
