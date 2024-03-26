7d4ca7c720139b4cd4159971fb546d2dd08eee50
Isox90 <step1kon@gmail.com>
2024-03-26 01:47:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+setTimeout(function () {
+    window.dispatchEvent(new Event('resize'));
+}, 1000);
+
 // Utwórz mapę
 const map = L.map('map').setView([50.264173, 18.890753], 10); // Przykładowe centrowanie na regionie Polski
 
